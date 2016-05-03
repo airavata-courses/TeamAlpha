f10ca8d4df61ec37f734401aa9f77da1a9d4d656
Ajinkya <adhamnas@indiana.edu>
2016-05-03 01:27:08 -0400

    Db Save module integration

--- Airavata_Remote_Job_Runner/src/main/java/com/teamAlpha/airavata/domain/User.java
+++ Airavata_Remote_Job_Runner/src/main/java/com/teamAlpha/airavata/domain/User.java
@@ -1,42 +1,61 @@
 package com.teamAlpha.airavata.domain;
 
 public class User {
-	
+
 	private int id;
+
+
 	private String username;
+
+
 	private String password;
+
+
 	private String role;
-	private boolean enabled;
+
+
+	private int enabled;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
-	public boolean isEnabled() {
+
+	public int getEnabled() {
 		return enabled;
 	}
-	public void setEnabled(boolean enabled) {
+
+	public void setEnabled(int enabled) {
 		this.enabled = enabled;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -53,7 +72,5 @@ public class User {
 		builder.append("]");
 		return builder.toString();
 	}
-	
-	
-	
+
 }
