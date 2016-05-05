74a2a9f36d85ebc2ce620b4da2e4f7087aa86434
Marlon Pierce <marpierc@iu.edu>
2016-05-05 14:29:20 -0400

    Need to start the service if it isn't running.

--- Airavata_Remote_Job_Runner/db/db_script_centos.sh
+++ Airavata_Remote_Job_Runner/db/db_script_centos.sh
@@ -4,6 +4,8 @@ if [ "$?" -gt "0" ]
  then
         sudo yum -y update
         sudo yum -y install postgresql-client postgresql postgresql-contrib
+        service postgresql initdb
+        service postgresql start
         sudo -u postgres psql postgres -a -f /opt/scripts/airavata_ddl_20160321.sql
         sudo -u postgres psql postgres -a -f /opt/scripts/airavata_dml_20160321.sql
         sudo -u postgres psql postgres -c "ALTER USER Postgres WITH PASSWORD 'root';"
